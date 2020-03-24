2db618f23b61c768e16d6a83d0ceec363c9ca16e
Maria Ines Aran <aranmariaines@gmail.com>
2020-03-24 10:36:09 -0300

    Add files via upload

---
+++ power_outage/data/company.js
@@ -0,0 +1,14 @@
+var data = {
+	fuente : 'Web Service',
+    empresa: 'EDENOR',
+    totalUsuariosSinSuministro: '453',
+    totalUsuariosConSuministro: '2.998.038',
+    ultimaActualizacion: '10:20',
+    totalUsuariosAyer: '18.504',
+    cortesPreventivos:  [],
+    cortesProgramados:  [],
+    cortesServicioMedia:  [{partido: 'MORENO',localidad: 'CUARTEL V',subestacion_alimentador: '263- / R:263-TR2 / 263-5525',usuarios: '402',normalizacion: '2020-03-24 14:00'}],
+    cortesComunicados:  [],
+    cortesServicioBaja:  [ {partido: 'CAPITAL FEDERAL', localidad: 'BELGRANO', usuarios: '13'}, {partido: 'MALVINAS ARGENTINAS', localidad: 'GRAND BOURG', usuarios: '21'}, {partido: 'MERLO', localidad: 'LIBERTAD', usuarios: '11'}, {partido: 'MORENO', localidad: 'MORENO', usuarios: '6'}]
+  }		
+		
